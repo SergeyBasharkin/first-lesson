d6ea26ded3e91a2254a912ede1a7c1248a9e6de3
Сергей Башаркин <basharkin@lanit.ru>
2026-06-06 11:16:16 +0300

    hello SkyPro

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет SkyPro!");
+        int a = 34;
+        int b = 322;
+
+        System.out.println(a + b);
     }
 }
